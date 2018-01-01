1f00a0cc757d2145caaf12a8ff7a067c63a075cf
quyet <quyet@whl.travel>
2018-01-01 15:57:21 +0700

    config

---
+++ Controller/Config/Index.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace TripFuser\MageSample\Controller\Config;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+
+    protected $helperData;
+
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \TripFuser\MageSample\Helper\Data $helperData
+
+    )
+    {
+        $this->helperData = $helperData;
+        return parent::__construct($context);
+    }
+
+    public function execute()
+    {
+
+        // TODO: Implement execute() method.
+
+        echo $this->helperData->getGeneralConfig('enable');
+        echo $this->helperData->getGeneralConfig('display_text');
+        exit();
+
+    }
+}
